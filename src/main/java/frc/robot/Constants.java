4b67c90f3752183389d194f1a48760bf0f14a8d8
beckzhouham <157067411+beckzhouham@users.noreply.github.com>
2024-07-16 09:16:03 -0400

    Update Constants.java
    added for extender, retractor, toggle

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,11 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public static class IntakeConstants {
+    public static final int rollerPort = 0;
+    public static final int pivotPort = 0;
+    public static final double stopPoint = 0;
+    public static final double startPoint = 0;
+  }
 }
