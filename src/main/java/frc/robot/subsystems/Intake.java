ead788e39b59c7a00b8c763174dec401f0c502e1
beckzhouham <157067411+beckzhouham@users.noreply.github.com>
2024-07-16 09:14:37 -0400

    Update Intake.java
    added extender, retractor, and toggle

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -28,3 +28,65 @@ public class Intake extends TimedRobot {
     } 
   }
 }
+
+
+
+
+public class Intake extends SubsystemBase {
+    boolean extended = false;
+    BooleanSupplier isExtended = () -> extended;
+    AbsoluteEncoder pivotEncoder;
+    CANSparkMax roller = new CANSparkMax(Constants.IntakeConstants.rollerPort, MotorType.kBrushless);
+    CANSparkMax pivot = new CANSparkMax(Constants.IntakeConstants.pivotPort, MotorType.kBrushless);
+
+    public Intake() {
+        pivotEncoder = pivot.getAbsoluteEncoder();
+    }
+// Toggles the extension motor
+    public Command toggleExtension() {
+        return runOnce(
+            () -> {
+                if (isExtended.getAsBoolean()) {
+                retract();
+                extended = false;
+            } else {
+                extend();
+                extended = true;
+            } 
+            }
+        );
+    }
+// Extends until a certain "angle" is reached
+    public Command extend() {
+        return runOnce(
+            () -> pivot.set(.5)
+        ).andThen(
+            //TODO
+            Commands.waitUntil(() -> pivotEncoder.getPosition() > Constants.IntakeConstants.stopPoint)
+            .andThen(() -> pivot.set(0))
+        );
+    }
+// Retracts until the original point
+    public Command retract () {
+        return runOnce(
+            () -> pivot.set(-.5)
+        ).andThen(
+            Commands.waitUntil(() -> pivotEncoder.getPosition() < Constants.IntakeConstants.startPoint)
+            .andThen(() -> pivot.set(0))
+        );
+
+    }
+// Starts the intake
+    public Command runIntake() {
+        return run(
+            () -> roller.set(1)
+        ).finallyDo(
+            () -> roller.set(0)
+        );
+    }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
