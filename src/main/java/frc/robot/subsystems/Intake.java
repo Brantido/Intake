2776ba680e7bfd382c0f1fd6e37c9c3e642a99ac
Brantido <bryantche0218@gmail.com>
2024-07-16 09:18:25 -0400

    Update Intake.java

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Intake extends TimedRobot {
   private final PWMSparkMax rollerMotor1 = new PWMSparkMax(0);  
   private final PWMSparkMax rollerMotor2 = new PWMSparkMax(1);  
-  private final PWMSparkMax rollerMotor3 = new PWMSparkMax(2);  
+  private final PWMSparkMax piviotMotor3 = new PWMSparkMax(2);  
       // Sets up and initializes the motor
 
   public void Update(XboxController controller) {
@@ -18,12 +18,12 @@ public class Intake extends TimedRobot {
       // Button is pressed and moving pivot motor to the target position
       rollerMotor1.set(0.5);
       rollerMotor2.set(0.5);
-      rollerMotor3.set(0.5);
+      piviotMotor3.set(0.5);
     } else {
       // Button is not pressed, turn off all motors
       rollerMotor1.set(0.0);
       rollerMotor2.set(0.0);
-      rollerMotor3.set(0.5);
+      piviotMotor3.set(-0.5);
 
     } 
   }
